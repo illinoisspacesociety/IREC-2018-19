7111b5721661aad26819269222f791fdacd65a21
Connor Latham <ConTron44@users.noreply.github.com>
2018-06-06 15:44:56 -0500

    Sensors code with good code for storing data for a 5 second test.

--- Hot_Fire_Code/Sensors_Code/Sensors_Code.ino
+++ Hot_Fire_Code/Sensors_Code/Sensors_Code.ino
@@ -1,34 +1,82 @@
+#include <SPI.h>
+#include "Adafruit_MAX31855.h"
+#include <SD.h>
+
 #define MAXDO 3 //Thermocouple DO on digital pin 3
 #define MAXCS 4 // Thermocouple CS on digital pin 4
 #define MAXCLK 5 // Thermocouple CLK on digital pin 5
 #define PT 17 // Pressure Transducer on analog pin 3 (17)
+#define SDCS 9 // SD Card CS is at 10
 
-float TRead // Temperature Read in C
-float time // Time values
-float PRead // Pressure Read in PSI
+float TRead; // Temperature Read in C
+float Time; // Time values
+float PRead; // Pressure Read in PSI
+bool Done;
+int testTime;
 
 Adafruit_MAX31855 thermocouple(MAXCLK, MAXCS, MAXDO);
+File TempFile;
+File PresFile;
+File TimeFile;
 
 void setup() {
+SD.remove("TempFile.txt");
+SD.remove("PresFile.txt");
+if(!SD.remove("TimeFile.txt")) {
+  Serial.println("What");
+}
+
+testTime = 5000;
 Serial.begin(9600);
+pinMode(SDCS,OUTPUT);
+if (!SD.begin(SDCS)) {
+    Serial.println("initialization failed!");
+    return;
+  }
 }
 
 void loop() {
-    delay(20) // Don't want this going too fast
-    // Let's get the time, Temperature, and Pressure
-    time = millis();
-    TRead = thermocouple.readCelsius();
-    PRead = PReadf();
-    // Let's print those
-    Serial.println("Here is Temperature: " + string(TRead))
-    Serial.println("Here is Pressure: " + string(PRead))
-    Serial.println("Here is Time: " + string(time))
-    // Now let's store them on the SD card
+    //delay(20); // Don't want this going too fast
+    // Let's get the time, Temperature, and Pressure, but only for some amount of time
+    if (Time < testTime) {
+      acquireData();
+    }
+    if(Done){
+      Serial.println("All done! Hope it went well.");
+      Done = false;
+    }
 }
 
 float PReadf() {
-    numRead = analogRead(PT);
-    milVRead = 5*(numRead/1023);
-    PRead = 2000*(pressureReadingInVolts-.498)/5.02;
-    return(PRead);
+    float numRead = analogRead(PT);
+    float milVRead = 5*(numRead/1023);
+    float PReadf = 2000*(milVRead-.498)/5.02;
+    return(PReadf);
 }
+
+void acquireData() {
+    Time = millis();
+    if(Time > testTime) {
+      Done = true;
+    }
+    TRead = thermocouple.readCelsius();
+    PRead = PReadf();
+    
+    // Let's print those
+    Serial.println("Here is Temperature: " + String(TRead));
+    Serial.println("Here is Pressure: " + String(PRead));
+    Serial.println("Here is Time: " + String(Time));
+    Serial.println();
+    
+    // Now let's store them on the SD card
+    TempFile = SD.open("TempFile.txt",FILE_WRITE);
+    TempFile.println(String(TRead));
+    TempFile.close();
+    PresFile = SD.open("PresFile.txt",FILE_WRITE);
+    PresFile.println(String(PRead));
+    PresFile.close();
+    TimeFile = SD.open("TimeFile.txt",FILE_WRITE);
+    TimeFile.println(String(Time));
+    TimeFile.close();
+}
+
